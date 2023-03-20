5cdc752d5f71a1847daa6cd0c31d7ab2062259b3
FlorianGoussin <florian.goussin@gmail.com>
2023-03-20 16:06:11 -0600

    more examples

--- perlin-noise2_graph/sketch.js
+++ perlin-noise2_graph/sketch.js
@@ -18,7 +18,17 @@ function draw() {
 
         // sin gives values between -1 and 1 so we push it by height/2 and then reajust the amplitude
         // let y = height/2 + sin(xOffset) * height/2; 
-        let y = noise(xOffset)*50 + height/2 + sin(xOffset) * height/4; 
+        // let y = noise(xOffset)*50 + height/2 + sin(xOffset) * height/4; 
+
+        // Mixing a wave form and perlin noise is a technique that can be applied for other things
+        // e.g. Make letters look more like hand writing
+        // const noiseVal = map(noise(xOffset), 0, 1, -50, 50); // Not dependend of the height so secondary here
+        // const sinVal = map(noise(xOffset), -1, 1, 0, height);
+
+        const noiseVal = map(noise(xOffset), 0, 1, 0, height); // Dependend of the height so primary here
+        const sinVal = map(noise(xOffset), -1, 1, -50, 50);
+        let y = noiseVal + sinVal;
+
         vertex(x, y);
         xOffset += increment;
     }
