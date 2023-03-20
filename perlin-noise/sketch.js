30bc859203989eca0b79b12dbde3d88c9bb9937d
FlorianGoussin <florian.goussin@gmail.com>
2023-03-20 00:20:25 -0600

    add a y random value with perlin noise picked from somewhere else in the graph

--- perlin-noise/sketch.js
+++ perlin-noise/sketch.js
@@ -1,7 +1,8 @@
 const WIDTH = 500;
 const HEIGHT = 500;
 
-let xOffset = 0;
+let xOffset1 = 0;
+let xOffset2 = 10000;
 
 function setup() {
     createCanvas(WIDTH, HEIGHT)
@@ -9,8 +10,11 @@ function setup() {
   
 function draw() {
     background(0);
-    const noiseVal = noise(xOffset);
-    const x = map(noiseVal, 0, 1, 0, WIDTH);
-    ellipse(x, HEIGHT/2, 40, 40);
-    xOffset += 0.01;
+    const noiseVal1 = noise(xOffset1);
+    const noiseVal2 = noise(xOffset2);
+    const x = map(noiseVal1, 0, 1, 0, WIDTH);
+    const y = map(noiseVal2, 0, 1, 0, WIDTH);
+    ellipse(x, y, 40, 40);
+    xOffset1 += 0.01;
+    xOffset2 += 0.01;
 }
