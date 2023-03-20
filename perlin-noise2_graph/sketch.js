bd205de1d84b8add92c3d71636bd4b7e81964f9d
FlorianGoussin <florian.goussin@gmail.com>
2023-03-20 13:15:06 -0600

    sin curve with added noise

--- perlin-noise2_graph/sketch.js
+++ perlin-noise2_graph/sketch.js
@@ -14,7 +14,11 @@ function draw() {
     beginShape();
     let xOffset = start;
     for (let x = 0; x < width; x++) {
-        let y = noise(xOffset) * height;
+        // let y = noise(xOffset) * height;
+
+        // sin gives values between -1 and 1 so we push it by height/2 and then reajust the amplitude
+        // let y = height/2 + sin(xOffset) * height/2; 
+        let y = noise(xOffset)*50 + height/2 + sin(xOffset) * height/4; 
         vertex(x, y);
         xOffset += increment;
     }
